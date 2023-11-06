f6d918fd63d024f02e332aeda5cb6cc639423c7d
bruno.alarcon <alarconbruno14@gmail.com>
2023-11-06 10:03:53 -0300

    agrego ejercicio 11

---
+++ js/ej11.js
@@ -0,0 +1,21 @@
+let nombres = [];
+let edades = [];
+
+
+for (let i = 1; i <= 3; i++) {
+  let nombre = prompt(`Ingrese el nombre ${i}:`);
+  let edad = parseInt(prompt(`Ingrese la edad de ${nombre}:`));
+
+  while (isNaN(edad)) {
+    edad = parseInt(prompt(`Ingrese una edad valida para ${nombre}:`));
+  }
+
+  nombres.push(nombre);
+  edades.push(edad);
+}
+
+let posMayor = edades.indexOf(Math.max(...edades));
+
+document.write(`<p>La persona mayor es: ${nombres[posMayor]}</p>`);
+
+
